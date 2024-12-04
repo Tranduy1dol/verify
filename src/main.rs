1bcade8e32f0846299c2ad85fd9d6ceb9c22f826
Tranduy1dol <tmd.id2004@gmail.com>
2024-12-04 16:42:38 +0700

    feat: f*ck docker compose

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
 use std::path::Path;
 use cairo_vm::types::layout_name::LayoutName;
 use prove_block::prove_block;
+use starknet::providers::jsonrpc::HttpTransport;
+use starknet::providers::{JsonRpcClient, Provider, Url};
 
 #[tokio::main]
 async fn main() {
     let compiled_os: &[u8] = include_bytes!("../build/os_latest.json");
 
-    let snos_url = "http://0.0.0.0:9545";
-    let block_number = 13;
+    let snos_url = "http://0.0.0.0:9944";
+    let provider = JsonRpcClient::new(HttpTransport::new(Url::parse(snos_url).unwrap()));
+    let block_number = provider.block_number().await.unwrap();
 
-    let (cairo_pie, snos_output) = prove_block(compiled_os, block_number, snos_url, LayoutName::starknet, false).await.unwrap();
-    cairo_pie.write_zip_file(Path::new("./cairo_pie.zip")).expect("failed to write");
+    eprintln!("block_number = {:#?}", block_number);
+
+    // let (cairo_pie, snos_output) = prove_block(compiled_os, block_number, snos_url, LayoutName::all_cairo, false).await.unwrap();
+    // cairo_pie.write_zip_file(Path::new("./cairo_pie.zip")).expect("failed to write");
 }
